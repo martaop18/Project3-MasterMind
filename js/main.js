46fa67a206109320235c046575afa25f923de8b4
marceop <marta.op18@gmail.com>
2023-05-28 20:59:12 +0200

    elimino carpetas de ayuda e innecesarias

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// SESSION STORAGE
+// SESSION STORAGE PLAYER
 
 const savePlayer = ()=>{
     let player = document.getElementById('playerName').value;
@@ -8,46 +8,6 @@ const savePlayer = ()=>{
     window.location.href = "../pages/gameOver.html"
     
 }
-
-
-
-
-
-
-
-// // RANDOM COLOR CREATION-EXPERT LEVEL
-
-// let secretCodeExpert = [];
-// const randomColorsE = ["blue", "orange", "green","yellow", "purple", "pink",];
-
-
-// for (i=0; i<4; i++){
-//     secretCodeExpert.push(randomColorsE[Math.floor(Math.random()*6)]);
-//     console.log(secretCodeExpert);
-// }
-// // RANDOM COLOR CREATION-ADVANCED LEVEL
-
-// let secretCodeAdvanced = [];
-// const randomColorsA = ["blue", "orange", "green","yellow", "purple"];
-
-
-// for (i=0; i<4; i++){
-//     secretCodeAdvanced.push(randomColorsA[Math.floor(Math.random()*5)]);
-//     console.log(secretCodeAdvanced);
-// }
-// // RANDOM COLOR CREATION-BEGINNER LEVEL
-
-// let secretCodeBeginner = [];
-// const randomColorsB = ["blue", "orange", "green", "yellow"];
-
-
-// for (i=0; i<4; i++){
-//     secretCodeBeginner.push(randomColorsB[Math.floor(Math.random()*4)]);
-//     console.log(secretCodeBeginner);
-// }
-
-
-// TO SHOW SECRET CODE COLORS FUNCTION
 
 
 
--- js/sstorage.js
+++
@@ -1,3 +0,0 @@
-
-let endGame = document.getElementById('wellDone!');
-endGame.innerHTML = `${sessionStorage.getItem('user')}`;
