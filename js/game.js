21c9ce1b6852e0832442355ec9eb5791971704d1
marceop <marta.op18@gmail.com>
2023-05-28 15:03:06 +0200

    empiezo a definir las propiedades del objeto con la plantilla class

--- js/game.js
+++ js/game.js
@@ -1,5 +1,32 @@
 // START THE GAME
-// DEFINING CONSTANS AND VARIABLES FOR THE GAME
+// DEFINING CONSTANS AND VARIABLES FOR THE  BEGGINER LEVEL GAME
+
+const myColors = ['#19A7CE','#F97B22','#00FFCA', '#F9F54B',];
+let secretCode = [];
+let staying = [];
+let rowCounter = 1;
+let colCounter = 1;
+let winner = false;
+
+// DEFINING ROW CLASS AND ITS METHODS
+class Row  {
+    constructor (){
+        this.turn = false;
+        this.spaces = [];
+    }
+}
+
+// / DEFINING ROW INSTANCES
+const row1 = new Row();
+const row2 = new Row();
+const row3 = new Row();
+const row4 = new Row();
+const row5 = new Row();
+const row6 = new Row();
+const row7 = new Row();
+const row8 = new Row();
+const row9 = new Row();
+const row10 = new Row();
 
 
 
