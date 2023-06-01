fc50ef2d7aebc91ee46b9fa1d618c5cbc1bd04ac
marceop <marta.op18@gmail.com>
2023-06-01 21:27:41 +0200

    añado colores seleccionados al tablero

--- js/colors.js
+++ js/colors.js
@@ -1,13 +1,13 @@
-
 const storeColors = () => {
-  const pickedColors = [];
-  colorInputs.forEach(input => {
-    pickedColors.push(input.value);
-  });
-  localStorage.setItem("pickedColors", JSON.stringify(pickedColors));
+  const colorInputs = document.querySelectorAll('input[type="color"]');
+  const arrayColores = Array.from(colorInputs).map(input => input.value);
+
+  sessionStorage.setItem("arrayColores", JSON.stringify(arrayColores));
+
 };
 
-
+const colorSection = document.getElementById("colorSection");
+colorSection.addEventListener("change", storeColors);
 
 
 
--- js/game.js
+++ js/game.js
@@ -1,12 +1,24 @@
 
-// GET PICKED COLORS
-const getstoredColors = () => {
-    const storedColors = localStorage.getItem('pickedColors');
-    window.location.href = "../pages/gameB.html"
+// GAME BOARD
 
-    // if(storedColors){
-    //     return JSON.parse(storedColors);
-    // }else{
-    //     return[];
-    // }
+const setColors = () => {
+    const span1 = document.getElementById("1");
+    const span2 = document.getElementById("2");
+    const span3 = document.getElementById("3");
+    const span4 = document.getElementById("4");
+
+
+
+    const arrayColores = JSON.parse(sessionStorage.getItem("arrayColores"));
+
+    if (arrayColores && arrayColores.length >=4) {
+        span1.style.backgroundColor = arrayColores[0];
+        span2.style.backgroundColor = arrayColores[1];
+        span3.style.backgroundColor = arrayColores[2];
+        span4.style.backgroundColor = arrayColores[3];
+    }
 }
+
+setColors();
+
+
