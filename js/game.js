1a6e4eaba39930a5ccbe3eb518f137fa6fd22030
marceop <marta.op18@gmail.com>
2023-06-03 23:46:12 +0200

    añado función cambi de valores colr rgb a hexadecimal

--- js/game.js
+++ js/game.js
@@ -79,3 +79,37 @@ console.log(currentCol, 'this  is the currect col')
 console.log(currentRow, 'this  is the currect row')
 currentCol = 1;
 }
+
+// DEFINING CONSTANT TO CHECK COMBINATION
+
+const checkCombination = () => {
+const col1 = document.getElementById(`fila${currentRow}-1`).style.backgroundColor;
+const col2 = document.getElementById(`fila${currentRow}-2`).style.backgroundColor;
+const col3 = document.getElementById(`fila${currentRow}-3`).style.backgroundColor;
+const col4 = document.getElementById(`fila${currentRow}-4`).style.backgroundColor;
+}
+
+// ADDING FUNCTION TO CHANGE COLOR VALUE FROM RGB TO HEX (COPIED FROM STACK OVERFLOW)
+
+function rgbToHex(rgb) {
+  const match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  if (!match) {
+    return rgb;
+  }
+
+  const hex = "#" + match.slice(1).map(component => parseInt(component).toString(16).padStart(2, '0')).join('');
+  return hex;
+}
+
+const col1ToHex = rgbToHex(col1);
+const col2ToHex = rgbToHex(col2);
+const col3ToHex = rgbToHex(col3);
+const col4ToHex = rgbToHex(col4);
+
+
+console.log(col1ToHex);
+console.log(col2ToHex);
+console.log(col3ToHex);
+console.log(col4ToHex);
+
+console.log('color 1-1', col1);
